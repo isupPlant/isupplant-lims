ae08140aba43a4d5824641384c9e48193edb0067
huodongsheng <huodongsheng@supcon.com>
2020-08-18 18:59:50 +0800

    新增手动计算逻辑

--- module_lims/src/main/java/com/supcon/mes/module_lims/utils/Util.java
+++ module_lims/src/main/java/com/supcon/mes/module_lims/utils/Util.java
@@ -1,6 +1,7 @@
 package com.supcon.mes.module_lims.utils;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 /**
  * Created by wanghaidong on 2020/7/16
@@ -36,5 +37,54 @@ public class Util {
         BigDecimal d2 = new BigDecimal(Integer.toString(1));
         // 四舍五入,保留2位小数
         return d1.divide(d2, 3, BigDecimal.ROUND_HALF_UP).toString();
+    }
+
+    public static boolean isNumeric(String str){
+        for (int i = str.length();--i>=0;){
+            if (!Character.isDigit(str.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 传入一个数列x计算平均值
+     * @param x
+     * @return 平均值
+     */
+    public static double average(List<Double> x) {
+        int n = x.size();            //数列元素个数
+        double sum = 0;
+        for (double i : x) {        //求和
+            sum+=i;
+        }
+        return sum/n;
+    }
+
+    /**
+     * 传入一个数列x计算方差
+     * 方差s^2=[（x1-x）^2+（x2-x）^2+......（xn-x）^2]/（n）（x为平均数）
+     * @param x 要计算的数列
+     * @return 方差
+     */
+    public static double variance(List<Double> x) {
+        int n = x.size();            //数列元素个数
+        double avg = average(x);    //求平均值
+        double var = 0;
+        for (double i : x) {
+            var += (i-avg)*(i-avg);    //（x1-x）^2+（x2-x）^2+......（xn-x）^2
+        }
+        return var/n;
+    }
+
+    /**
+     * 传入一个数列x计算标准差
+     * 标准差σ=sqrt(s^2)，即标准差=方差的平方根
+     * @param x 要计算的数列
+     * @return 标准差
+     */
+    public static double standardDiviation(List<Double> x) {
+        return  Math.sqrt(variance(x));
     }
 }
